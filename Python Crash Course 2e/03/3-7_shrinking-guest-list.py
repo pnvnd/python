54f17efe3c9565a89d6ecc4a2c1918d264a0d646
Peter Nguyen <peter@datacrunch.ca>
2020-07-26 15:01:59 -0400

    Updated code

--- Python Crash Course 2e/03/3-7_shrinking-guest-list.py
+++ Python Crash Course 2e/03/3-7_shrinking-guest-list.py
@@ -21,14 +21,15 @@ for guest in guests:
 print("\nWe have no room for guests at the dinner party.")
 
 # Remove guests from the end of the list until 2 guests are left
-for guest in guests:
-    while len(guests) > 2:
-        print(f"Sorry, {guests.pop()}!  We have no more room at the dinner party.")
+
+while len(guests) > 2:
+    print(f"Sorry, {guests.pop()}!  We have no more room at the dinner party.")
 
 # Message for remaining guests
 for guest in guests:
     print(f"Hello, {guest}!  You are invited to a dinner party.")
 
+# Remove guests from guestlist and print guestlist
 while len(guests) > 0:
     del guests[0]
     print(guests)
