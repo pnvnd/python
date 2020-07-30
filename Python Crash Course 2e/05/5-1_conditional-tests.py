e3f9e0ee00b6f39face9e367f9ece3aa3d9ce428
pnvnd <peter@datacrunch.ca>
2020-07-29 22:55:10 -0400

    Initial commit

---
+++ Python Crash Course 2e/05/5-1_conditional-tests.py
@@ -0,0 +1,9 @@
+favorite_number = 69
+print(favorite_number == 21)
+print(favorite_number != 21)
+print(favorite_number >= 21)
+print(favorite_number <= 21)
+
+favorite_cars = ["Lancer", "WRX", "Avalon"]
+print("WRX" in favorite_cars)
+print("wrx" in favorite_cars)
---
+++ Python Crash Course 2e/05/5-2_more-conditional-tests.py
@@ -0,0 +1,12 @@
+favorite_number = 69
+print(favorite_number == 21 or favorite_number == 43)
+print(favorite_number != 21 and favorite_number != 43)
+print(favorite_number >= 21)
+print(favorite_number <= 21)
+
+favorite_cars = ["Lancer", "WRX", "Avalon"]
+print("WRX" in favorite_cars)
+print("wrx" in favorite_cars)
+
+mine = favorite_cars[1].lower()
+print(mine in favorite_cars)
